381e9d45e353c89a16aa66800fb32ddb9ba37958
Your Name <you@example.com>
2023-03-20 20:28:28 -0300

    ultima modificacion

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-console.log("aca mando yo")
+const express = require('express'); /*traigo la constante de la libreria*/
+const app = express(); /*asigno el valor en la constante app*/
+const PORT= 3001;
+app.get('/api', (req, res)=>{ res.json({mensaje:'chau mundo'}) .status(200)});
+app.listen (PORT, ()=>{console.log('estoy escuchando en el puerto: '+ PORT)});
